11ec9a8fd983b148f627c15e13658bc98128c0f4
jyotik <jyotik1608@gmail.com>
2020-05-30 14:00:25 +0530

    luv2code.com

---
+++ 08-spring-rest/solution-code-spring-crm-rest-demo-delete-customer/sql-scripts/02-customer-tracker.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE  IF NOT EXISTS `web_customer_tracker` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `web_customer_tracker`;
+-- MySQL dump 10.13  Distrib 5.6.13, for osx10.6 (i386)
+--
+-- Host: 127.0.0.1    Database: web_customer_tracker
+-- ------------------------------------------------------
+-- Server version	5.6.16
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+/*create table Customer(id int(11),first_name varchar(30),last_name varchar(30),email varchar(35));*/
---
+++ 08-spring-rest/solution-code-spring-crm-rest-demo-delete-customer/sql-scripts/insert-values.sql
@@ -0,0 +1,3 @@
+insert into customer values(101,"aman","bhatiya","aman@mail.com");
+insert into customer values(102,"anu","roy","anu@mail.com");
+select * from customer;
